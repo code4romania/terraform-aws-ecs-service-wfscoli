69db48c9e2a29b6a35e011f7c43db2079b62d0cf
Andrei Ioniță <hi@andrei.io>
2025-05-12 13:33:05 +0100

    wip

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "ecs_app" {
   source  = "code4romania/ecs-service/aws"
-  version = "0.1.2"
+  version = "0.1.3"
 
   namespace    = var.name
   cluster_name = var.common.ecs_cluster.cluster_name
